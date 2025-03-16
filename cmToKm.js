67788a972406f391dc4f634b03574fbaaeb400f5
Faisal Saputra <faisalsaputra57@gmail.com>
2025-03-16 13:54:47 +0900

    Memperbaiki file yang kosong dengan menambahkan kode

---
+++ cmToKm.js
@@ -0,0 +1,7 @@
+function cmToKm(cm) {
+    const km = cm / 100000;
+    return `${km} km`;
+  }
+  
+ console.log(cmToKm(100000)); // output: 1 km
+  
---
+++ fahrenheitToCelsius.js
@@ -0,0 +1,7 @@
+function fahrenheitToCelsius(fahrenheit) {
+    const celsius = (fahrenheit - 32) * 5/9;
+    return celsius;
+  }
+  
+  console.log(fahrenheitToCelsius(100)); // output: 37.77777777777778
+  
